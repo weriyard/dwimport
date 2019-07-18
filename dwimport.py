8c84f779b79f0c9655de1cd7ce880939d85caf80
adasieq <adamer.sk@gmail.com>
2019-07-18 16:33:08 +0200

    add progres informations

--- dwimport.py
+++ dwimport.py
@@ -20,9 +20,7 @@ def scan_for_data(data_path):
 
 def process_file(filepath):
     with codecs.open(filepath, 'r', 'utf-8') as fd:
-        print 'PARESE FILE: ', filepath
         parsed = parse_content(fd.read())
-
     return parsed
 
 
@@ -36,25 +34,28 @@ def parse_content(content):
     for table in tables:
         for table_row in table.findAll('tr'):
             columns = table_row.findAll('td')
-            print 'col->', columns
+            # print 'col->', columns
             if len(columns) == 0:
                 continue
             if len(columns) == 1:
-                labels = columns=[0]
+                label = columns[0].text
                 value = ""
             elif len(columns) > 2:
-                label = columns=[0]
-                value = ' '.join(columns[1:])
+                label = columns[0].text
+                value = ' '.join([c.text for c in columns[1:]])
             else:
                 label, value = columns
-                label = label.text.replace(':', '')
+                label = label.text
                 value = value.text
             x = 1
             while label in labels:
                 label = u"{label}_{counter}".format(label=label, counter=x)
                 x += 1
             labels.append(label)
-            output_rows.append((unicode(label), unicode(value)))
+            output_rows.append((unicode(label
+                                        .replace(':', '')
+                                        .strip()
+                                        .capitalize()), unicode(value.strip())))
 
     return OrderedDict(output_rows)
 
@@ -71,25 +72,39 @@ def process_file2csv(filepath):
 
 def main():
     files = scan_for_data(DATA_DIR)
-    print 'FILES NUMBER:', len(files)
+    files_count = len(files)
+    print 'FILES NUMBER:', files_count
     big_data = OrderedDict()
     workbook = xlsxwriter.Workbook('data.xlsx')
     worksheet = workbook.add_worksheet()
+
+    header_counter = OrderedDict()
+
+    print 'PARSE DATA FROM FILES:',
+    cnt = 0
     for f in files:
+        print f,
         parsed_data = process_file(f)
 
         for item in parsed_data:
             if item not in big_data:
+                header_counter[item] = 1
+                print '\nNEW COL:', item,
                 big_data[item] = []
+            header_counter[item] += 1
             big_data[item].append(parsed_data[item])
+        cnt += 1
+        print "\r\t\t{}/{} {:04.2f}%".format(cnt, files_count, (float(cnt) / files_count) * 100),
+
     pos = 0
     for l in big_data:
         to_write = [l]
         to_write.extend(big_data[l])
-        print to_write
         worksheet.write_column(0, pos, to_write)
         pos += 1
     workbook.close()
+
+    print header_counter
 
 
 if __name__ == '__main__':
