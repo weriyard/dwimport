2d0ecc4907f0a98ee40b873f555b7ca2ee12c953
adasieq <adamer.sk@gmail.com>
2019-07-19 16:36:23 +0200

    improve export: * add source id file * fix bad postions for data in excel * add table source name

--- dwimport.py
+++ dwimport.py
@@ -4,6 +4,7 @@ import codecs
 import csv
 import glob
 import os
+import sys
 from bs4 import BeautifulSoup
 from collections import OrderedDict
 
@@ -31,10 +32,24 @@ def parse_content(content):
 
     output_rows = []
     labels = []
+    table_type_names = []
     for table in tables:
+
+        try:
+            table_type = table.findPrevious("h4").text
+        except AttributeError:
+            table_type = "_"
+
+        y = 1
+        while table_type in table_type_names:
+            table_type = "{table_type}_{counter}".format(table_type=table_type,
+                                                         counter=y)
+            y += 1
+
+        table_type_names.append(table_type)
+
         for table_row in table.findAll('tr'):
             columns = table_row.findAll('td')
-            # print 'col->', columns
             if len(columns) == 0:
                 continue
             if len(columns) == 1:
@@ -51,12 +66,12 @@ def parse_content(content):
             while label in labels:
                 label = u"{label}_{counter}".format(label=label, counter=x)
                 x += 1
+            label = u"{label} [{type}]".format(label=label, type=table_type)
             labels.append(label)
             output_rows.append((unicode(label
                                         .replace(':', '')
                                         .strip()
                                         .capitalize()), unicode(value.strip())))
-
     return OrderedDict(output_rows)
 
 
@@ -70,10 +85,14 @@ def process_file2csv(filepath):
             writer.writerows(parsed)
 
 
+0
+
+
 def main():
     files = scan_for_data(DATA_DIR)
+    # files = ['./data/bogusz_mariola.txt']
     files_count = len(files)
-    print 'FILES NUMBER:', files_count
+    print >> sys.stderr, 'FILES NUMBER:', files_count
     big_data = OrderedDict()
     workbook = xlsxwriter.Workbook('data.xlsx')
     worksheet = workbook.add_worksheet()
@@ -82,19 +101,33 @@ def main():
 
     print 'PARSE DATA FROM FILES:',
     cnt = 0
+    big_data['FILE ID'] = []
     for f in files:
-        print f,
+        print f
         parsed_data = process_file(f)
-
+        big_data['FILE ID'].append(f)
+        tmp_labels_list = ['FILE ID']
         for item in parsed_data:
             if item not in big_data:
                 header_counter[item] = 1
-                print '\nNEW COL:', item,
-                big_data[item] = []
+                print u"NEW COL:;  {name:30} ;[{file}] ;({cnt})".format(name=item,
+                                                                        file=f,
+                                                                        cnt=cnt)
+                if cnt == 0:
+                    big_data[item] = []
+                else:
+                    empty_list = [""] * cnt
+                    big_data[item] = empty_list
             header_counter[item] += 1
+            tmp_labels_list.append(item)
             big_data[item].append(parsed_data[item])
+        all_lables = set(big_data.keys())
+        labels_diff = all_lables.difference(set(tmp_labels_list))
+        for lb in labels_diff:
+            big_data[lb].append("")
+
         cnt += 1
-        print "\r\t\t{}/{} {:04.2f}%".format(cnt, files_count, (float(cnt) / files_count) * 100),
+        # print "\r\t\t{}/{} {:04.2f}%".format(cnt, files_count, (float(cnt) / files_count) * 100),
 
     pos = 0
     for l in big_data:
@@ -104,7 +137,7 @@ def main():
         pos += 1
     workbook.close()
 
-    print header_counter
+    # print header_counter
 
 
 if __name__ == '__main__':
